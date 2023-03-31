2aadd4c35bdfc8f32c2fe053cccae74c3e80d7c3
Christopher Leung <chrisleung360@yahoo.com>
2023-03-31 12:11:51 -0700

    save work

--- client.py
+++ client.py
@@ -1,5 +1,23 @@
 import socket
 import stdiomask
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.asymmetric.dh import DHParameters, DHPrivateKey, DHPublicKey, DHParameterNumbers
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
+
+class Client:
+    def __init__(self, params=None, priv=None, shared=None):
+        self.params = params
+        self.priv = priv
+        self.shared = shared
+
+    def get_params(self): return self.get_params
+
+    def get_priv(self): return self.priv
+
+    def get_shared(self): return self.shared
+
 
 def main():
     print("---Client---")
@@ -18,7 +36,7 @@ def client_handler(email, password):
     client_socket.connect(server_address)
 
     # send the message to the server
-    message = email+','+password
+    message = email + ',' + password
     client_socket.sendall(message.encode())
 
     # receive the response from the server
@@ -29,5 +47,63 @@ def client_handler(email, password):
     client_socket.close()
 
 
+def client_pre_connect(client_socket):
+    # connect the socket to the server's IP address and port
+    server_address = ('localhost', 8888)
+    client_socket.connect(server_address)
+
+    # send the message to the server
+    message = "Hello, Server!"
+    client_socket.sendall(message.encode())
+
+    # receive the response from the server
+    response = client_socket.recv(1024).decode()
+    print(f'[Client.py]: Received message: {response}')
+
+    if response != "Hello, Client!":
+        print(f'[DEBUG]: ERROR - Did not receive correct connect handshake from server')
+        client_socket.close()
+    else:
+        print(f'[DEBUG]: Valid handshake received!')
+
+
+def client_post_connect(client_socket: socket):
+    print(f'[DEBUG]: --- Generating Shared Secret ---')
+
+    print(f'[DEBUG]: Receiving prime (p) from server')
+    p: int = client_socket.recv(2048).decode()
+    print(f'[DEBUG]: Received prime {p} from server')
+
+    print(f'[DEBUG]: Receiving generator (g) from server')
+    g: int = client_socket.recv(2048).decode()
+    print(f'[DEBUG]: Received generator {g} from server')
+
+    print(f'[DEBUG]: Receiving public key (pk_client) from server')
+    pk_client: int = client_socket.recv(2048).decode()
+
+    params: DHParameterNumbers = dh.DHParameterNumbers(p, g)
+    client_secret: DHPrivateKey = params.generate_private_key()
+    client_public: DHPublicKey = client_secret.public_key()
+
+    # shared_secret = client_secret.exchange(pk_client)
+
+
+def test_client():
+    print("---Client---")
+
+    # create a TCP/IP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish stable connection handshake
+    client_pre_connect(client_socket)
+
+    # perform DH Key Exchange
+    shared_secret = client_post_connect(client_socket)
+
+    print(f'[Client]: Closing connection')
+    client_socket.close()
+
+
 if __name__ == "__main__":
-    main()
+    test_client()
+#    main()
--- server.py
+++ server.py
@@ -1,17 +1,77 @@
 import socket
+import pickle
+import pydevd_pycharm
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.asymmetric.dh import DHParameters, DHPrivateKey, DHPublicKey
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes, serialization
 
-# create a TCP/IP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind the socket to a specific IP address and port
-server_address = ('localhost', 8888)
-server_socket.bind(server_address)
+# pydevd_pycharm.settrace('localhost', port=8888, stdoutToServer=True, stderrToServer=True)
+class Server:
+    def __init__(self, params=None, priv=None, shared=None):
+        self.params: DHParameters = params
+        self.priv: DHPrivateKey = priv
+        self.shared: DHPublicKey = shared
 
-# listen for incoming connections
-server_socket.listen(1)
-print('Server is listening on', server_address)
+    def get_params(self): return self.get_params
 
-while True:
+    def get_priv(self): return self.priv
+
+    def get_shared(self): return self.shared
+
+
+class Public_Packet:
+    def __init__(self, params=None, shared=None, message=None):
+        self.params: DHParameters = params
+        self.shared = shared
+        self.message = message
+
+    def get_params(self): return self.get_params
+
+    def get_shared(self): return self.shared
+
+
+def main():
+    # create a TCP/IP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # bind the socket to a specific IP address and port
+    server_address = ('localhost', 8888)
+    server_socket.bind(server_address)
+
+    # listen for incoming connections
+    server_socket.listen(1)
+    print('Server is listening on', server_address)
+
+    while True:
+        # wait for a connection
+        client_socket, client_address = server_socket.accept()
+        print('[Server]: Received connection from', client_address)
+
+        # receive the message from the client
+        message = client_socket.recv(1024).decode()
+        print('[Server]: Received message:', message)
+        separate = message.split(',')
+
+        # send the response back to the client
+        response = 'Hello, ' + separate[0]
+        client_socket.sendall(response.encode())
+        print('[Server]: Sent response: \"{response}\"')
+
+        # close the connection
+        client_socket.close()
+
+
+def serv_pre_connect(server_socket: socket):
+    # bind the socket to a specific IP address and port
+    server_address = ('localhost', 8888)
+    server_socket.bind(server_address)
+
+    # listen for incoming connections
+    server_socket.listen(1)
+    print('Server is listening on', server_address)
+
     # wait for a connection
     client_socket, client_address = server_socket.accept()
     print('[Server]: Received connection from', client_address)
@@ -19,12 +79,82 @@ while True:
     # receive the message from the client
     message = client_socket.recv(1024).decode()
     print('[Server]: Received message:', message)
-    separate = message.split(',')
 
-    # send the response back to the client
-    response = 'Hello, ' + separate[0]
-    client_socket.sendall(response.encode())
-    print('[Server]: Sent response: \"{response}\"')
+    if message != "Hello, Server!":
+        print(f'[DEBUG]: ERROR - Did not receive correct connect handshake from client')
+        client_socket.close()
+        server_socket.close()
+    else:
+        # send the response back to the client
+        print(f'[DEBUG]: Valid handshake received!')
+        response = 'Hello, Client!'
+        client_socket.sendall(response.encode())
+        print(f'[Server]: Sent response: \"{response}\"')
 
-    # close the connection
-    client_socket.close()
+    return client_socket
+
+
+def crypto_service():
+    params = dh.generate_parameters(generator=2, key_size=1024)
+    priv_key = params.generate_private_key()
+    shared_secret = params.generate_private_key().public_key()
+
+    return Server(params, priv_key, shared_secret)
+
+
+def serv_post_connect(serv: Server, client: socket):
+    print(f'[DEBUG]: Generating Shared Secret')
+    print(f'[DEBUG]: Sending parameters')
+    temp1 = serv.params.parameter_numbers()
+    pk_server = serv.priv.public_key().public_numbers().y
+    p = temp1.p
+    q = temp1.g
+
+    print(f'[DEBUG]: Sending prime (p) to client')
+    client.sendall(bytes(p))
+    print(f'[DEBUG]: Sent: {p} to client')
+
+    print(f'[DEBUG]: Sending generator (g) to client')
+    client.sendall(bytes(q))
+    print(f'[DEBUG]: Sent: {q} to client')
+
+    print(f'[DEBUG]: public key (pk_server) to client')
+    client.sendall(bytes(pk_server))
+    print(f'[DEBUG]: Sent: {pk_server} to client')
+
+
+
+
+
+def test_server():
+    print("---Server---")
+
+    serv = crypto_service()
+
+    # create a TCP/IP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # attempt to accept client
+    client: socket = serv_pre_connect(server_socket)
+
+    # DH key exchange
+    serv_post_connect(serv, client)
+
+    print(f'[Server]: Closing connection')
+    server_socket.close()
+
+
+def dhell():
+    params = dh.generate_parameters(generator=2, key_size=1024)
+    priv1 = params.generate_private_key()
+    pub1 = priv1.public_key()
+
+    temp1 = params.parameter_numbers()
+    temp2 = temp1.p
+    temp3 = temp1.g
+
+
+if __name__ == "__main__":
+#    dhell()
+    test_server()
+#    main()
